d9bd1fd5c68e532ff1b3e9ddb8a05eb8cd81cd43
Squid <16716@totton.ac.uk>
2022-06-30 15:26:42 +0100

    fix the jump pads vice!!!

--- Dimensionality Project/Assets/Scripts/Jump pad/JumpPadScript.cs
+++ Dimensionality Project/Assets/Scripts/Jump pad/JumpPadScript.cs
@@ -7,6 +7,7 @@ public class JumpPadScript : MonoBehaviour
     Rigidbody rb;
     [SerializeField] private Animator Boing_Animator;
     public float power = 200f;
+    public AudioSource jumpPadSound;
 
     private float coolDown = 0.1f;
     private float cooldowntime;
@@ -23,6 +24,9 @@ public class JumpPadScript : MonoBehaviour
             rb.AddForce(transform.up * power, ForceMode.Impulse);
             Boing_Animator.SetTrigger("Boing");
             cooldowntime = time + coolDown;
+
+            jumpPadSound.Play();
+
         }
     }
 }
